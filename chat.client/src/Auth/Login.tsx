eb7547c3cffca986b4dfc02040c8f354e3cddd3f
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-06-28 16:37:20 +0200

    Working on the LoginForm

--- chat.client/src/Auth/Login.tsx
+++ chat.client/src/Auth/Login.tsx
@@ -1,8 +1,12 @@
+import LoginForm from "../components/LoginForm";
+
 export default function Login()
 {
     return(
         <>
-        
+            <div className="flex flex-col items-center justify-center">
+                <LoginForm/>
+            </div>
         </>
     );
 }
--- chat.client/src/main.tsx
+++ chat.client/src/main.tsx
@@ -1,11 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './Auth/Login';
 
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
-      <Route/>
+      <Route path='login' element={<Login/>}/>
     </Routes>
   </BrowserRouter>
 )
