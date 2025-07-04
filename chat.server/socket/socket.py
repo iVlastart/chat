592c635d6011280cddeb6d0f4b22f4c2af06100b
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-07-04 16:27:35 +0200

    now the .env vars are loading in my py script

--- chat.server/socket/socket.py
+++ chat.server/socket/socket.py
@@ -1,11 +1,16 @@
-import socket as sock #🧦
 from dotenv import load_dotenv
+import os
+import socket as sock #🧦
 
 load_dotenv()
+HOSTNAME = os.getenv('HOSTNAME')
+PORT = os.getenv('PORT')
 
 def main():
     with sock.socket(sock.AF_INET, sock.SOCK_STREAM) as s:
-        s.bind(())
+        s.bind((HOSTNAME, PORT))
+        s.listen()
+        conn, addr = s.accept()
 
 if __name__ == '__main__':
     main()
